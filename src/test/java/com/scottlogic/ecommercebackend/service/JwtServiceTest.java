652acadc604ec5bb6f03bc47503dd229f4b91c12
Zidan Wang <zwang@scottlogic.com>
2025-10-01 15:40:49 +0100

    adds unit tests for EncryptionService and JwtService

---
+++ src/test/java/com/scottlogic/ecommercebackend/service/JwtServiceTest.java
@@ -0,0 +1,31 @@
+package com.scottlogic.ecommercebackend.service;
+
+import com.scottlogic.ecommercebackend.model.LocalUser;
+import com.scottlogic.ecommercebackend.model.dao.LocalUserDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JwtServiceTest {
+
+    @Autowired
+    private JWTService jwtService;
+    @Autowired
+    private LocalUserDAO localUserDAO;
+
+    @Test
+    public void testVerificationTokenNotUsableForLogin(){
+        LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
+        String token = jwtService.generateVerificationJWT(user);
+        Assertions.assertNull(jwtService.getUsername(token), "Verification token should not contain username.");
+    }
+
+    @Test
+    public void testAuthTokenReturnsUsername(){
+        LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
+        String token = jwtService.generateJWT(user);
+        Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for auth should contain users username.");
+    }
+}
