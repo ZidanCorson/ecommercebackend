af2cd78577f9b9feed48879ab43901d3d06f1d26
Zidan Wang <zwang@scottlogic.com>
2025-10-07 09:44:22 +0100

    adds Address putting and patching in controller

---
+++ src/main/java/com/scottlogic/ecommercebackend/model/dao/AddressDAO.java
@@ -0,0 +1,11 @@
+package com.scottlogic.ecommercebackend.model.dao;
+
+import com.scottlogic.ecommercebackend.model.Address;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+
+public interface AddressDAO extends ListCrudRepository<Address, Long> {
+
+    List<Address> findByUser_Id(long id);
+}
